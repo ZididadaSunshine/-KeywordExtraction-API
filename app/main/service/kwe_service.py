caba7730611291f830281a8e1df33a06f10b2e91
Matias Jensen <sejemus@yahoo.dk>
2018-12-06 10:28:37 +0100

    Debug

--- app/main/service/kwe_service.py
+++ app/main/service/kwe_service.py
@@ -10,5 +10,7 @@ def extract_keywords(data):
     if not data or 'text' not in data:
         return KWEServiceResponse.Error
     else:
+        print("Recieved data: ")
+        print(f"----\n{data['text']}\n----")
         extractor = TKGExtractor(data['text'])
         return dict(keywords=extractor.extract_n_keywords(n=15)), KWEServiceResponse.Success
