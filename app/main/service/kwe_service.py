9f1cd216f29d514f2fa0bd513c1f713bf3f89cb6
Matias Jensen <1457902+OfficialHisha@users.noreply.github.com>
2018-12-06 12:01:18 +0100

    Removed debug output

--- app/main/service/kwe_service.py
+++ app/main/service/kwe_service.py
@@ -7,7 +7,5 @@ class KWEServiceResponse:
 
 
 def extract_keywords(corpus):
-    print("Recieved data: ")
-    print(f"----\n{corpus}\n----")
     extractor = TKGExtractor(corpus)
     return dict(keywords=extractor.extract_n_keywords(n=15)), 200
